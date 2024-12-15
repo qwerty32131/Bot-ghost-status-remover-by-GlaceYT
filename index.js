da8ad9edb1fe7c3d2dd5537bede6cf981fe96f8e
Delideli112332 <michal01.deli01@gmail.com>
2024-12-15 14:20:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*
+  /*
 
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
                                                  
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["sra do gara", "kowal chuj"];
+const statusMessages = [" ", " "];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
